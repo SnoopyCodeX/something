1ed1211bb49d464635c80c9dbf6143aaa3534d97
SnoopyCodeX <extremeclasherph@gmail.com>
2023-11-30 02:29:35 +0800

    Update

---
+++ gen/CommitGenerator.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class CommitGenerator {
+    public static final void main(String[] args) {
+        new Thread(() -> {
+            try {
+                File folder = new File("gen");
+                
+                if (!folder.exists()) {
+                    folder.mkdir();
+                }
+
+                File fe = new File("README.yml");
+                FileWriter fw = new FileWriter(fe);
+                BufferedWriter bufferedWriter = new BufferedWriter(fw);
+
+                bufferedWriter.write(new Date().toString());
+                bufferedWriter.close();
+                fw.close();
+                
+                System.out.println("Successfully generate file");
+            } catch (Exception en) {
+                System.out.println("Failed to generate file: " + en.getMessage());
+            }
+        }).start();
+    }
+}
